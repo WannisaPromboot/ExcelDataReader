9c90f2aa528a31ed045ed96463f29edc7ecba03b
58030218 <58030218@kmitl.ac.th>
2018-12-07 20:12:44 +0700

    add graph

--- DisplayChartExcel/Form1.Designer.cs
+++ DisplayChartExcel/Form1.Designer.cs
@@ -28,19 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.Button1 = new System.Windows.Forms.Button();
             this.Button2 = new System.Windows.Forms.Button();
             this.firstRowNamesCheckBox = new System.Windows.Forms.CheckBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.sheetCombo = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(41, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.Size = new System.Drawing.Size(291, 20);
             this.textBox1.TabIndex = 0;
             // 
             // Button1
@@ -51,6 +58,7 @@
             this.Button1.TabIndex = 1;
             this.Button1.Text = "open";
             this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Button2
             // 
@@ -60,6 +68,7 @@
             this.Button2.TabIndex = 2;
             this.Button2.Text = "Process";
             this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // firstRowNamesCheckBox
             // 
@@ -79,11 +88,58 @@
             this.dataGridView1.Size = new System.Drawing.Size(384, 360);
             this.dataGridView1.TabIndex = 4;
             // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // sheetCombo
+            // 
+            this.sheetCombo.FormattingEnabled = true;
+            this.sheetCombo.Location = new System.Drawing.Point(211, 38);
+            this.sheetCombo.Name = "sheetCombo";
+            this.sheetCombo.Size = new System.Drawing.Size(121, 21);
+            this.sheetCombo.TabIndex = 5;
+            this.sheetCombo.SelectedIndexChanged += new System.EventHandler(this.sheetCombo_SelectedIndexChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(459, 78);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(329, 347);
+            this.zedGraphControl1.TabIndex = 7;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.sheetCombo);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.firstRowNamesCheckBox);
             this.Controls.Add(this.Button2);
@@ -92,6 +148,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,6 +162,11 @@
         private System.Windows.Forms.Button Button2;
         private System.Windows.Forms.CheckBox firstRowNamesCheckBox;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox sheetCombo;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
     }
 }
 
--- DisplayChartExcel/Form1.cs
+++ DisplayChartExcel/Form1.cs
@@ -1,20 +1,154 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using ExcelDataReader;
+using System.Diagnostics;
+using ZedGraph;
+using System.Drawing;
 
 namespace DisplayChartExcel
 {
     public partial class Form1 : Form
     {
+        private DataSet ds;
         public Form1()
         {
             InitializeComponent();
+            CreateChart(zedGraphControl1);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private static IList<string> GetTablenames(DataTableCollection tables)
+        {
+            var tableList = new List<string>();
+            foreach (var table in tables)
+            {
+                tableList.Add(table.ToString());
+            }
+
+            return tableList;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            var extension = Path.GetExtension(textBox1.Text).ToLower();
+            using (var stream = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+                IExcelDataReader reader = null;
+                if (extension == ".xls")
+                {
+                    reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                }
+                else if (extension == ".xlsx")
+                {
+                    reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                }
+                else if (extension == ".csv")
+                {
+                    reader = ExcelReaderFactory.CreateCsvReader(stream);
+                }
+
+                if (reader == null)
+                    return;
+
+                var openTiming = sw.ElapsedMilliseconds;
+                // reader.IsFirstRowAsColumnNames = firstRowNamesCheckBox.Checked;
+                using (reader)
+                {
+                    ds = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        UseColumnDataType = false,
+                        ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = firstRowNamesCheckBox.Checked
+                        }
+                    });
+                }
+
+                toolStripStatusLabel1.Text = "Elapsed: " + sw.ElapsedMilliseconds.ToString() + " ms (" + openTiming.ToString() + " ms to open)";
+
+                var tablenames = GetTablenames(ds.Tables);
+                sheetCombo.DataSource = tablenames;
+
+                if (tablenames.Count > 0)
+                    sheetCombo.SelectedIndex = 0;
+
+                UpdateZedGraph(zedGraphControl1);
+                // dataGridView1.DataSource = ds;
+                // dataGridView1.DataMember
+            }
+        }
+
+        private void SelectTable()
+        {
+            var tablename = sheetCombo.SelectedItem.ToString();
+
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = ds; // dataset
+            dataGridView1.DataMember = tablename;
+
+            // GetValues(ds, tablename);
+        }
+
+        private void sheetCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectTable();
+        }
+
+        public void CreateChart(ZedGraphControl zgc)
+        {
+            GraphPane myPane = zgc.GraphPane;
+            myPane.Title.Text = "Number";
+            myPane.XAxis.Title.Text = "Number";
+            myPane.YAxis.Title.Text = "Value";
+            PointPairList list = new PointPairList();
+            LineItem myCurve = myPane.AddCurve("Data",
+               list, Color.ForestGreen, SymbolType.Diamond);
+            zgc.AxisChange();
+        }
+
+        private void UpdateZedGraph(ZedGraphControl zedGraphControl1)
+        {
+
+            if (zedGraphControl1.GraphPane.CurveList.Count <= 0)
+                return;
+
+ 
+            LineItem curve = zedGraphControl1.GraphPane.CurveList[0] as LineItem;
+            if (curve == null)
+                return;
+
+            IPointListEdit list = curve.Points as IPointListEdit;
+   
+            if (list == null)
+                return;
+
+            list.Clear();
+            int maxrows = ds.Tables[0].Rows.Count;
+            Console.WriteLine("ds.Tables[0].Rows.Count = {0}", maxrows);
+
+            for (int i = 0; i < maxrows; i++)
+            {
+                var a = ds.Tables[0].Rows[i];
+                double x = (double)a.ItemArray.GetValue(0);
+                double y = (double)a.ItemArray.GetValue(1);
+                list.Add(x, y);
+            }
+        
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
         }
     }
 }
